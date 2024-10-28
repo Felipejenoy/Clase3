b4a735b8c42041adbb9de72f73d765f1cf1f975c
Felipejenoy <johanordonez@unicauca.edu.co>
2024-10-27 19:40:42 -0500

    Subiendo mi proyecto

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
-import os
-
-# Configuración de la cadena de conexión usando variables de entorno
-SERVER = os.getenv('DB_SERVER', 'appweb-crud-db-server.database.windows.net')
-DATABASE = os.getenv('DB_DATABASE', 'WebApp-Crud-db')
-USERNAME = os.getenv('DB_USERNAME', 'crud_paas')
-PASSWORD = os.getenv('DB_PASSWORD', 'U12345678-')
-DRIVER = os.getenv('DB_DRIVER', 'ODBC Driver 18 for SQL Server')
+# Configuración de la cadena de conexión
+SERVER = 'appweb-crud-db-server.database.windows.net'  # Nombre del servidor
+DATABASE = 'WebApp-Crud-db'                             # Nombre de la base de datos
+USERNAME = 'crud_paas'                                  # Nombre de usuario
+PASSWORD = 'U12345678-'                                 # Contraseña
+DRIVER = 'ODBC Driver 18 for SQL Server'                # Controlador ODBC
 
 # Cadena de conexión
-connectionString = f'DRIVER={{{DRIVER}}};SERVER={SERVER};PORT=1433;DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
+connectionString = f'DRIVER={DRIVER};SERVER={SERVER};PORT=1433;DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
